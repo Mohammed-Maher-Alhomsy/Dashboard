307ae525441c139062c38c83b55e43fafa86dd21
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2023-08-23 03:11:55 +0300

    create Calendar, Customers, Employees, Orders, and start with Kanban Page

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ const App = () => {
           </div>
 
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white z-50">
               <Sidebar />
             </div>
           ) : (
